2b4d16a45e151e4f41ca37850c3a250d90f105de
mohammedalbehairy <mohammedalbehairy90@gmail.com>
2020-02-03 02:17:51 +0200

    created project modules

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthLayoutComponent } from './components/auth-layout/auth-layout.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+
+@NgModule({
+  declarations: [AuthLayoutComponent, LoginComponent, RegisterComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
---
+++ src/app/users/users.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UsersRoutingModule } from './users-routing.module';
+import { UsersLayoutComponent } from './components/users-layout/users-layout.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+@NgModule({
+  declarations: [UsersLayoutComponent, HomeComponent],
+  imports: [
+    CommonModule,
+    UsersRoutingModule
+  ]
+})
+export class UsersModule { }
